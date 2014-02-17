d1640a0aa5acbcbc30753aca34c8a74637db3e1b
Ben Hyrman <ben.hyrman@gmail.com>
2014-02-17 16:13:03 -0500

    added view and static handler

---
+++ 01_views_and_static_files/WhatTheNancy.Tests/StaticContentTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Testing;
+using Shouldly;
+
+namespace WhatTheNancy.Tests
+{
+	public class StaticContentTests
+	{
+		public void can_fetch_css_because_css_is_cool()
+		{
+			var sut = new Browser(new Bootstrapper());
+
+			var result = sut.Get("/css/site.css");
+
+			result.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+		}
+	}
+}
---
+++ 01_views_and_static_files/WhatTheNancy/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Conventions;
+
+namespace WhatTheNancy
+{
+	public class Bootstrapper : DefaultNancyBootstrapper
+	{
+		protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions)
+		{
+			base.ConfigureConventions(nancyConventions);
+
+			nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/css", "content/css"));
+		}
+	}
+}
